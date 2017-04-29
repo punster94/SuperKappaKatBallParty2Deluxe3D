6b95f5d1dd076af04f9a04cb2c4f164923674534
stropheum <stropheum@yahoo.com>
2017-04-28 20:55:37 -0400

    Fleshed out spawner API

--- Source/Game.Shared/PowerupSpawner.cpp
+++ Source/Game.Shared/PowerupSpawner.cpp
@@ -1,16 +1,46 @@
 #include "pch.h"
 #include "PowerupSpawner.h"
+#include <random>
 
 
 namespace KatBall
 {
-	PowerupSpawner::PowerupSpawner()
+	PowerupSpawner::PowerupSpawner():
+		mSpawnLocation(glm::vec4(0)), mSpawnChance(0.0f)
 	{
-		(*this)["Location"].SetStorage(&mLocation, 1);
+		(*this)["SpawnLocation"].SetStorage(&mSpawnLocation, 1);
 		(*this)["SpawnChance"].SetStorage(&mSpawnChance, 1);
 	}
 
-	PowerupSpawner::~PowerupSpawner()
+	glm::vec4 PowerupSpawner::GetSpawnLocation()
 	{
+		return mSpawnLocation;
+	}
+
+	void PowerupSpawner::SetSpawnLocation(const glm::vec4& spawnLocation)
+	{
+		mSpawnLocation = spawnLocation;
+	}
+
+	float PowerupSpawner::GetSpawnChance()
+	{
+		return mSpawnChance;
+	}
+
+	void PowerupSpawner::SetSpawnChance(const float spawnChance)
+	{
+		mSpawnChance = spawnChance;
+	}
+
+	void PowerupSpawner::AttemptSpawn()
+	{
+		std::default_random_engine generator;
+		std::uniform_int_distribution<std::uint32_t> distribution(0, 100);
+		std::uint32_t roll = distribution(generator);
+
+		if (roll <= mSpawnChance)
+		{
+			// TODO: Pick a random powerup to spawn based on spawn weights, at mSpawnLocation
+		}
 	}
 }
--- Source/Game.Shared/PowerupSpawner.h
+++ Source/Game.Shared/PowerupSpawner.h
@@ -7,13 +7,35 @@ namespace KatBall
 	{
 	public:
 
+		/// Constructor
 		PowerupSpawner();
 		
-		~PowerupSpawner();
+		/// Destructor (defaulted)
+		~PowerupSpawner() = default;
+
+		/// Accessor method for the powerup spawner's spawn location
+		/// @Return: The location that the powerups from this object will spawn
+		glm::vec4 GetSpawnLocation();
+
+		/// Mutator method for the powerup spawner's spawn location
+		/// @Param spawnLocation: The new location from which powerups will spawn from this object
+		void SetSpawnLocation(const glm::vec4& spawnLocation);
+
+		/// Accessor method for the powerup spawner's spawn chance
+		/// @Return: The chance that this object will spawn a powerup
+		float GetSpawnChance();
+
+		/// Mutator method for the powerup spawner's spawn chance
+		/// @Param spawnChance: The chance that this object will spawn a powerup
+		void SetSpawnChance(const float spawnChance);
+
+		/// Rolls for chance to spawn, and on success, instantiates a powerup at the spawn location
+		/// Rolls are between 1 and 100. If roll is <= spawnChance, a powerup is spawned
+		void AttemptSpawn();
 
 	private:
-		glm::vec4 mLocation;
-		float mSpawnChance;
+		glm::vec4 mSpawnLocation; // Location that powerups will be spawned from this object
+		float mSpawnChance; // Chance between 0.0 and 100.0 that this spawner will generate an item
 	};
 }
 
