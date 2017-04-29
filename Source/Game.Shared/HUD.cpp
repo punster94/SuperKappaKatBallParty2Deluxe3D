4dca17960f39819aa07285fc28f3d5879b4c3f59
jmoser <justinm1093@gmail.com>
2017-04-29 13:43:05 -0400

    Adding files for HUD.

---
+++ Source/Game.Shared/HUD.cpp
@@ -0,0 +1,9 @@
+
+#include "pch.h"
+#include "HUD.h"
+
+using namespace FieaGameEngine;
+using namespace KatBall;
+using namespace std;
+
+//
---
+++ Source/Game.Shared/HUD.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+#include "Entity.h"
+
+namespace KatBall
+{
+	//
+}
